502487b63046a5114356041c2f85fb313daf8d0a
bradygrapentine <brady.grapentine@gmail.com>
2021-05-19 19:43:15 -0400

    done with explorer mode

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
 # Scoreboard
 
 ![SDG](./docs/button.png)
+
+<!-- Explorer Mode -->
+
+Part 1:
+
+Create a new project using the beta stack and implement the design using the given HTML & CSS.
+
+<!-- done -->
+
+Update the CSS to improve the styling based on your own design and tastes.
+
+<!-- done -->
+
+Avoid updating the HTML too much, try to use existing elements and classes. However, do add new classes or elements for your design or your JavaScript if needed.
+
+<!-- done -->
+
+Assign an event to the input fields to automatically change the team's name as the user types.
+
+<!-- done -->
+
+Assign click events to the buttons to update the corresponding values on the page. The score buttons should adjust the current score by a "hard-coded" value.
+
+<!-- done -->
+
+Deploy your site.
+
+<!-- done -->
+
+Part 2:
+
+Add an if statement to stop each team's score from going below 0.
+
+<!-- done -->
+
+Add an if statement to stop each team's score from going above 21.
+When a team gets 21 points, display a message to the user of who won.
+
+<!-- done -->
+
+Add a reset button that resets the scoreboard back to each team having 0 points.
+
+<!-- done -->
+
+<!-- Adventure Mode -->
+
+Add more CSS to the winning teams score to indicate who won.
+
+Implement a Quarter/Period Tracker that would be appropriate for the sport.
+
+Add some styling to make it more presentable. Have fun with it.
+
+Make the layout responsive.
+
+<!-- Epic Mode -->
+
+Add a Timer that counts down automatically and updates the period/quarter appropriately.
+
+Save/load the information to local storage.
+
+Add anything else that your sport would have on its scoreboard (e.g., fouls, sacks, shots on goal).
+
+Improve the ease of use for score keeping in your sport.
--- public/main.js
+++ public/main.js
@@ -1,7 +1,160 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let adjustScore = 1
+
+function plusButtonClickTeam1(event) {
+  const team1Score = document.querySelector('.team1 .score')
+  const team2Score = document.querySelector('.team2 .score')
+  const team1Name = document.querySelector('.team1 h2')
+  const team2Name = document.querySelector('.team2 h2')
+
+  if (Number(team2Score.textContent) >= 21) {
+    window.alert(`Game Over. ${team2Name.textContent} won. You lost`)
+    return
   }
+  if (Number(team1Score.textContent) >= 21) {
+    window.alert(`Game Over. You, ${team1Name.textContent}, won.`)
+    return
+  }
+
+  const current = Number(Number(team1Score.textContent) + adjustScore)
+  team1Score.textContent = current
+
+  if (Number(team1Score.textContent) === 21) {
+    window.alert(`You, ${team1Name.textContent}, won!`)
+  }
+  console.log(event.target.value)
+  console.log(event)
+}
+function plusButtonClickTeam2(event) {
+  const team2Score = document.querySelector('.team2 .score')
+  const team1Score = document.querySelector('.team1 .score')
+  const team2Name = document.querySelector('.team2 h2')
+  const team1Name = document.querySelector('.team1 h2')
+
+  if (Number(team1Score.textContent) >= 21) {
+    window.alert(`Game Over. ${team1Name.textContent} won. You lost`)
+    return
+  }
+  if (Number(team2Score.textContent) >= 21) {
+    window.alert(`Game Over. You, ${team2Name.textContent}, won.`)
+    return
+  }
+
+  const current = Number(Number(team2Score.textContent) + adjustScore)
+  team2Score.textContent = current
+  if (Number(team2Score.textContent) === 21) {
+    window.alert(`You, ${team2Name.textContent}, won!`)
+  }
+
+  console.log(event.target.value)
+  console.log(event)
+}
+function minusButtonClickTeam1(event) {
+  const team1Score = document.querySelector('.team1 .score')
+  const team2Score = document.querySelector('.team2 .score')
+  const team2Name = document.querySelector('.team2 h2')
+  const team1Name = document.querySelector('.team1 h2')
+
+  if (Number(team2Score.textContent) >= 21) {
+    window.alert(`Game Over. ${team2Name.textContent} won. You lost`)
+    return
+  }
+  if (Number(team1Score.textContent) >= 21) {
+    window.alert(`Game Over. You, ${team1Name.textContent}, won.`)
+    return
+  }
+
+  if (Number(team1Score.textContent) <= 0) {
+    window.alert('Invalid request')
+    return
+  }
+  const current = Number(Number(team1Score.textContent) - adjustScore)
+  team1Score.textContent = current
+
+  console.log(event.target.value)
+  console.log(event)
+}
+function minusButtonClickTeam2(event) {
+  const team2Score = document.querySelector('.team2 .score')
+  const team1Score = document.querySelector('.team1 .score')
+  const team2Name = document.querySelector('.team2 h2')
+  const team1Name = document.querySelector('.team1 h2')
+
+  if (Number(team1Score.textContent) >= 21) {
+    window.alert(`Game Over. ${team1Name.textContent} won. You lost`)
+    return
+  }
+  if (Number(team2Score.textContent) >= 21) {
+    window.alert(`Game Over. You, ${team2Name.textContent}, won.`)
+    return
+  }
+  if (Number(team2Score.textContent) <= 0) {
+    window.alert('Invalid request')
+    return
+  }
+  const current = Number(Number(team2Score.textContent) - adjustScore)
+  team2Score.textContent = current
+
+  console.log(event.target.value)
+  console.log(event)
+}
+function updateTeam1Name(event) {
+  const teamName = document.querySelector('.team1 h2')
+  teamName.textContent = event.target.value
+  if (teamName.textContent === '') {
+    teamName.textContent = 'Team 1'
+  }
+  console.log(event.target.value)
+  console.log(event)
+}
+
+function updateTeam2Name(event) {
+  const teamName = document.querySelector('.team2 h2')
+  teamName.textContent = event.target.value
+  if (teamName.textContent === '') {
+    teamName.textContent = 'Team 2'
+  }
+  console.log(event.target.value)
+  console.log(event)
+}
+
+function clickToResetScoreboard(event) {
+  const team1Score = document.querySelector('.team1 .score')
+  const team2Score = document.querySelector('.team2 .score')
+  team1Score.textContent = 0
+  team2Score.textContent = 0
+  console.log(event.target.value)
+  console.log(event)
+}
+
+function main() {
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', updateTeam1Name)
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', updateTeam2Name)
+  document
+    .querySelector('.team1 i.add')
+    .addEventListener('click', plusButtonClickTeam1)
+  document
+    .querySelector('.team1 i.subtract')
+    .addEventListener('click', minusButtonClickTeam1)
+  document
+    .querySelector('.team2 i.add')
+    .addEventListener('click', plusButtonClickTeam2)
+  document
+    .querySelector('.team2 i.subtract')
+    .addEventListener('click', minusButtonClickTeam2)
+  document
+    .querySelector('fieldset i.reset')
+    .addEventListener('click', clickToResetScoreboard)
 }
 
 document.addEventListener('DOMContentLoaded', main)
+
+// Assign an event to the input fields
+// to automatically change the team's name as the user types.
+
+// Assign click events to the buttons to update the corresponding
+// values on the page. The score buttons should adjust the current
+// score by a "hard-coded" value.
